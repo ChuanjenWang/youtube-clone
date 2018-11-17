728a91ad1556e4d74cf771afa936ee13620e1468
Spencer.Wang <chuanjen.wang@gmail.com>
2018-11-18 01:17:28 +0800

    Updates

---
+++ precache-manifest.6eb4b3c26dac95404054bc2b12295e12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcc2e6b6f9d9b750e6d7",
+    "url": "/youtube-clone/static/js/runtime~main.dcc2e6b6.js"
+  },
+  {
+    "revision": "2e420e23785705dca506",
+    "url": "/youtube-clone/static/js/main.2e420e23.chunk.js"
+  },
+  {
+    "revision": "0bf4eb032cf0f3fd4f8a",
+    "url": "/youtube-clone/static/js/1.0bf4eb03.chunk.js"
+  },
+  {
+    "revision": "2e420e23785705dca506",
+    "url": "/youtube-clone/static/css/main.58a82076.chunk.css"
+  },
+  {
+    "revision": "2816119ad3d9103cde378f375cd0fda2",
+    "url": "/youtube-clone/index.html"
+  }
+];
